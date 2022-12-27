abcd955e404346837ffb55856ac7ca3e0f75ef4d
higton <matpaulo@hotmail.com.br>
2022-12-27 08:44:21 -0300

    improve the quiz to create a final answer

--- src/main.pl
+++ src/main.pl
@@ -9,35 +9,53 @@ menu :-
     menu_option(Choice).
 
 % handle the menu options
-menu_option(1) :- ask_questions(1).
+menu_option(1) :- start_quiz.
 menu_option(2) :- exit.
 menu_option(_):- write('Não é uma opção válida\n\n'), nl, menu.
 
-ask_questions(N) :-
-    question(N, Text, Choices, Correct),
+% list of possible combinations and the related team
+teams([
+    (3, 2, 3, 'São Paulo!'),
+    (3, 1, 3, 'Flamengo!')
+]).
+
+% final answer
+answer(X, Y, Z, Answer) :-
+    teams(Teams),
+    member((X, Y, Z, Team), Teams),
+    !, % cut to stop searching for more solutions
+    string_concat('Você é torcedor do time do ', Team, Temp1),
+    string_concat(Temp1, '!', Answer).
+answer(_, _, _, 'Desculpe, mas não encontramos um time de futebol para você.').
+
+% start the quiz and compute the final answer
+start_quiz :-
+    ask_questions(1, X, Y, Z),
+    answer(X, Y, Z, Answer),
+    write(Answer), nl.
+
+ask_questions(N, X, Y, Z) :-
+    question(N, Text, Choices),
     write(Text), nl,
     write_choices(Choices, 1),
-    read(Answer),
-    check_answer(Answer, Correct),
+    read(Response),
+    (   N =:= 1
+    ->  X = Response
+    ;   N =:= 2
+    ->  Y = Response
+    ;   N =:= 3
+    ->  Z = Response
+    ),
     Next is N + 1,
-    ask_questions(Next).
-ask_questions(_) :-
-    write('Quiz completed.'), nl, exit.
+    ask_questions(Next, X, Y, Z).
+ask_questions(_, _, _, _).
 
-% write the multiple choice options
+% write the choices for a question
 write_choices([H|T], N) :-
     write(N), write('. '), write(H), nl,
     Next is N + 1,
     write_choices(T, Next).
 write_choices([], _) :- nl.
-
-% check the answer and print the result
-check_answer(Answer, Correct) :-
-    Answer =:= Correct,
-    write('Correct!'), nl.
-check_answer(Answer, Correct) :-
-    Answer =\= Correct,
-    write('Incorrect.'), nl.
 
 % exit the program
 exit :- halt.
--- src/questions.pl
+++ src/questions.pl
@@ -1,3 +1,3 @@
-% define the quiz questions and answers
-question(1, 'What is your favorite color?', ['Green', 'Blue', 'Red', 'Yellow'], 1).
-question(2, 'What is your age?', ['21', '23', '32', '35'], 2).
+question(1, 'Qual sua região preferida do Brasil?', ['Norte', 'Nordeste', 'Sudeste', 'Sul']).
+question(2, 'Qual sua cidade preferida no Brasil?', ['Rio de Janeiro', 'São Paulo', 'Belo Horizonte', 'Porto Alegre']).
+question(3, 'Qual sua cor preferida?', ['Verde', 'Amarelo', 'Vermelho', 'Azul']).
