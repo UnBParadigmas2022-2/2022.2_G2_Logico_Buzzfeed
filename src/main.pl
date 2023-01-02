38e38a4d80cc152f9359004b15e23f06cf1ac518
VitorLamego <vitorlamegoo@gmail.com>
2023-01-02 00:58:14 -0300

    Transformando respostas em lista

--- src/main.pl
+++ src/main.pl
@@ -1,6 +1,10 @@
 % load the quiz questions from a file
 :- consult('questions.pl').
 
+% start the program
+:- initialization(menu).
+
+% BuzzFeed Menu
 menu :-
     write('Bem-vindo ao Quiz! Escolha uma das opções abaixo:'), nl,
     write('1 - Iniciar o quiz'), nl,
@@ -12,6 +16,26 @@ menu :-
 menu_option(1) :- start_quiz.
 menu_option(2) :- exit.
 menu_option(_):- write('Não é uma opção válida\n\n'), nl, menu.
+
+% start the quiz and compute the final answer
+start_quiz :-
+    ask_questions(1, [], NewList),
+    answer(X, Y, Z, Answer),
+    write(Answer), nl, exit.
+
+ask_questions(N, Answers, NewList) :-
+    question(N, Text, Choices),
+    write(Text), nl,
+    write_choices(Choices, 1),
+    read(Response),
+    insert_answer(Answers, Response, Z),
+    answers_list(N, NewList, Z),
+    Next is N + 1,
+    ask_questions(Next, Z, NewList).
+ask_questions(_, _, _).
+
+answers_list(3, NewList, Z) :- NewList = Z.
+answers_list(_, _, _).
 
 % list of possible combinations and the related team
 teams([
@@ -28,28 +52,6 @@ answer(X, Y, Z, Answer) :-
     string_concat(Temp1, '!', Answer).
 answer(_, _, _, 'Desculpe, mas não encontramos um time de futebol para você.').
 
-% start the quiz and compute the final answer
-start_quiz :-
-    ask_questions(1, X, Y, Z),
-    answer(X, Y, Z, Answer),
-    write(Answer), nl, exit.
-
-ask_questions(N, X, Y, Z) :-
-    question(N, Text, Choices),
-    write(Text), nl,
-    write_choices(Choices, 1),
-    read(Response),
-    (   N =:= 1
-    ->  X = Response
-    ;   N =:= 2
-    ->  Y = Response
-    ;   N =:= 3
-    ->  Z = Response
-    ),
-    Next is N + 1,
-    ask_questions(Next, X, Y, Z).
-ask_questions(_, _, _, _).
-
 % write the choices for a question
 write_choices([H|T], N) :-
     write(N), write('. '), write(H), nl,
@@ -57,8 +59,12 @@ write_choices([H|T], N) :-
     write_choices(T, Next).
 write_choices([], _) :- nl.
 
+% manipulate list
+
+append_list([],L,L).
+append_list([X1|L1],L2,[X1|L3]) :- append_list(L1,L2,L3).
+
+insert_answer(L, X, NewL) :- append_list(L, [X], NewL).
+
 % exit the program
 exit :- halt.
-
-% start the program
-:- initialization(menu).
